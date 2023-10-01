c23b246ffd42142a58869d24742a82534e7fbaa3
zarina-0-0 <b.zarina_2601@mail.ru>
2023-10-01 19:15:35 +0300

    second

--- JSFile.js
+++ JSFile.js
@@ -40,3 +40,22 @@ backBtn.addEventListener("click", () => {
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollLeft -= 900;
 });
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
